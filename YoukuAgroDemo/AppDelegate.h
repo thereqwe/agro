59a951ec6dc414d348be02e92279abcc56149f79
thereqwe@qq.com <3454453q>
2016-08-25 11:18:54 +0800

    Initial Commit

---
+++ YoukuAgroDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YoukuAgroDemo
+//
+//  Created by Yue Shen on 16/8/25.
+//  Copyright © 2016年 Yue Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YoukuAgroDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YoukuAgroDemo
+//
+//  Created by Yue Shen on 16/8/25.
+//  Copyright © 2016年 Yue Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
